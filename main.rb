b250da9086b2420cd50785713a892803a1221dc9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-14 18:52:44 -0700

    feat: validate user input on load or new game request

--- main.rb
+++ main.rb
@@ -2,3 +2,10 @@
 
 puts 'Welcome to a CLI version of the historical game of Chess!'
 puts 'Please type in "load" or "new" to load or begin a new game, respectively.'
+
+input = gets.chomp
+
+until %w[load new].include?(input)
+  puts 'Invalid input!'
+  input = gets.chomp
+end
