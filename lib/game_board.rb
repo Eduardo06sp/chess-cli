0cb721f339ec9e594f315ba348ddde5ac67e6c50
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-14 20:54:57 -0700

    test: create 64 hash elements for board in GameBoard

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -2,5 +2,18 @@
 
 class GameBoard
   def initialize
+    @board = create_board
+  end
+
+  def create_board
+    x_axis = ('a'..'h').to_a
+    y_axis = (1..8).to_a
+    board = {}
+
+    x_axis.each do |letter|
+      y_axis.each { |num| board["#{letter}#{num}"] = ' ' }
+    end
+
+    board
   end
 end
