52db2ed4c79c6128d89e3cc5bda04c0ae6858b00
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-16 19:22:09 -0700

    feat: add movement directions

--- lib/king.rb
+++ lib/king.rb
@@ -4,5 +4,15 @@ class King
   def initialize(color)
     @color = color
     @legal_moves = nil
+    @movement_directions = [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+      [-1, -1],
+      [-1, 0],
+      [-1, 1]
+    ]
   end
 end
