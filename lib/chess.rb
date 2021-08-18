cb7a352be8c0d6135db017406227045d774df454
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-17 20:03:41 -0700

    feat: initialize with player two

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Chess
-  def initialize(player_one)
+  def initialize(player_one, player_two)
     @player_one = player_one
+    @player_two = player_two
   end
 end
