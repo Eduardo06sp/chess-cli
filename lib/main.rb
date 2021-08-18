b170bd40fdc7256fb68e5d6da16db91705ee9312
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-18 07:19:00 -0700

    feat: request player names, use default if none provided

--- lib/main.rb
+++ lib/main.rb
@@ -11,4 +11,11 @@ until %w[load new].include?(input)
 end
 
 if input == 'new'
+  puts 'Please enter a name for player one, or press enter to use the default.'
+  input = gets.chomp
+  p1_name = input == ' ' ? 'Player 1' : input
+
+  puts 'Please enter a name for player two, or press enter to use the default.'
+  input = gets.chomp
+  p2_name = input == ' ' ? 'Player 2' : input
 end
