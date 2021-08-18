6362c94189b51c2c58c8f8adb7eb9628465e11ac
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-17 22:56:12 -0700

    feat: add getter method for both player instance variables

--- lib/chess.rb
+++ lib/chess.rb
@@ -3,6 +3,8 @@
 require_relative 'game_board'
 
 class Chess
+  attr_reader :player_one, :player_two
+
   def initialize(player_one, player_two)
     @player_one = player_one
     @player_two = player_two
