cc1114b9bb30179777ea242e29ebceba0e943098
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-17 22:51:31 -0700

    test: add instance doubles to instantiate new Chess object

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -3,7 +3,9 @@
 require_relative '../lib/chess'
 
 describe Chess do
-  subject(:new_game) { Chess.new }
+  let(:player_one) { instance_double(Player) }
+  let(:player_two) { instance_double(Player) }
+  subject(:new_game) { Chess.new(player_one, player_two) }
 
   describe '#fill_king_rank' do
   end
