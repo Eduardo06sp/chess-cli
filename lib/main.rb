e45a5a1760d0fe95ac403c74624e37cf2775ac5b
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-18 19:22:51 -0700

    fix: check if user entered empty string instead of space for player names

--- lib/main.rb
+++ lib/main.rb
@@ -13,9 +13,9 @@ end
 if input == 'new'
   puts 'Please enter a name for player one, or press enter to use the default.'
   input = gets.chomp
-  p1_name = input == ' ' ? 'Player 1' : input
+  p1_name = input == '' ? 'Player 1' : input
 
   puts 'Please enter a name for player two, or press enter to use the default.'
   input = gets.chomp
-  p2_name = input == ' ' ? 'Player 2' : input
+  p2_name = input == '' ? 'Player 2' : input
 end
