eeaa43fabb5a67263e7ea72654d4800a77be48f8
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-18 19:45:27 -0700

    feat: require chess and player files

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative 'chess'
+require_relative 'player'
+
 puts 'Welcome to a CLI version of the historical game of Chess!'
 puts 'Please type in "load" or "new" to load or begin a new game, respectively.'
 
