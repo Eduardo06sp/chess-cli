1db941ace4c63cf176ff50fc27aaa273168557a9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-29 22:20:02 -0700

    feat: add attr_accessor for moved variable

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Pawn
-  attr_accessor :legal_moves
+  attr_accessor :legal_moves, :moved
   attr_reader :type, :color, :movement_directions
 
   def initialize(color)
