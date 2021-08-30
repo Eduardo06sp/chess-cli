618f12738b33396c08a1e01eb7769c059bc7b115
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-08-29 19:08:08 -0700

    test: add context for #attacking_pieces_locations tests

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -134,5 +134,7 @@ describe Chess do
   end
 
   describe '#attacking_pieces_locations' do
+    context 'when d1 King is moved to f4 and enemy pieces are moved to attacking positions' do
+    end
   end
 end
