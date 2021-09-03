e382d447fb194e120519df019d6ba2440dcabde5
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-09-02 20:26:38 -0700

    test: expect #available_moves to return d4 and e2 if h4 Queen attack d4 King

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -334,6 +334,18 @@ describe Chess do
 
         expect(available_pieces).to eq(%w[d4 e3])
       end
+
+      it 'returns d4 and e2 if h4 Queen attacks d4 King' do
+        queen = new_game.game_board.board['d8'].value
+        new_game.game_board.move_piece(queen, 'd8', 'h4')
+        new_game.generate_legal_moves('h4')
+
+        white_pieces = new_game.locate_player_pieces('white')
+        available_pieces = new_game.available_pieces(white_pieces)
+        new_game.update_legal_moves('white')
+
+        expect(available_pieces).to eq(%w[d4 e2])
+      end
     end
   end
 end
