4246691a539bef5cef92846b4b38bc742d7231e4
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-09-10 22:53:43 -0700

    fix: only add check-blocking pieces to available pieces if only one piece attacks King

--- lib/chess.rb
+++ lib/chess.rb
@@ -104,17 +104,16 @@ class Chess
       check_blocking_pieces = check_blocking_pieces(king_location, player_pieces, attacking_pieces_locations)
 
       available_pieces = if attacking_pieces_locations.count == 1
-                           player_pieces.select do |space|
+                           (player_pieces.select do |space|
                              piece = game_board.board[space].value
 
                              piece.type == 'King' ||
                                piece.legal_moves.include?(attacking_pieces_locations[0])
-                           end
+                           end) + check_blocking_pieces
                          else
                            [king_location]
                          end
 
-      available_pieces += check_blocking_pieces
     else
       available_pieces = player_pieces.select do |space|
         piece = game_board.board[space].value
