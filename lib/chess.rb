72c4312563245dcd3f986399e594f650703a77aa
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-09-15 22:17:37 -0700

    refactor: move files for chess pieces to separate folder

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'game_board'
-require_relative 'rook'
-require_relative 'knight'
-require_relative 'bishop'
-require_relative 'queen'
-require_relative 'king'
-require_relative 'pawn'
+require_relative 'pieces/rook'
+require_relative 'pieces/knight'
+require_relative 'pieces/bishop'
+require_relative 'pieces/queen'
+require_relative 'pieces/king'
+require_relative 'pieces/pawn'
 
 class Chess
   attr_accessor :turn
