5f452b75a0d73089738ceb4f3d3515ce0ae97796
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-09-16 20:51:35 -0700

    feat: display empty chess board with x and y values

--- lib/terminal_interface.rb
+++ lib/terminal_interface.rb
@@ -10,4 +10,19 @@ module TerminalInterface
       ---------------------------------------
     HEREDOC
   end
+
+  def display_board
+    puts <<~HEREDOC
+        A B C D E F G H
+      8 \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[0m 8
+      7 \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[0m 7
+      6 \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[0m 6
+      5 \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[0m 5
+      4 \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[0m 4
+      3 \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[0m 3
+      2 \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[0m 2
+      1 \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[45m  \e[47m  \e[0m 1
+        A B C D E F G H
+    HEREDOC
+  end
 end
