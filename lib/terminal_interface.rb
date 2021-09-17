4b1061b8f18c6e2c090d9437ff469084c0909fab
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-09-16 21:18:14 -0700

    feat: set board foreground color to black

--- lib/terminal_interface.rb
+++ lib/terminal_interface.rb
@@ -14,14 +14,14 @@ module TerminalInterface
   def display_board
     puts <<-HEREDOC
             A B C D E F G H
-          8 \e[47m#{val('a8')} \e[45m#{val('b8')} \e[47m#{val('c8')} \e[45m#{val('d8')} \e[47m#{val('e8')} \e[45m#{val('f8')} \e[47m#{val('g8')} \e[45m#{val('h8')} \e[0m 8
-          7 \e[45m#{val('a7')} \e[47m#{val('b7')} \e[45m#{val('c7')} \e[47m#{val('d7')} \e[45m#{val('e7')} \e[47m#{val('f7')} \e[45m#{val('g7')} \e[47m#{val('h7')} \e[0m 7
-          6 \e[47m#{val('a6')} \e[45m#{val('b6')} \e[47m#{val('c6')} \e[45m#{val('d6')} \e[47m#{val('e6')} \e[45m#{val('f6')} \e[47m#{val('g6')} \e[45m#{val('h6')} \e[0m 6
-          5 \e[45m#{val('a5')} \e[47m#{val('b5')} \e[45m#{val('c5')} \e[47m#{val('d5')} \e[45m#{val('e5')} \e[47m#{val('f5')} \e[45m#{val('g5')} \e[47m#{val('h5')} \e[0m 5
-          4 \e[47m#{val('a4')} \e[45m#{val('b4')} \e[47m#{val('c4')} \e[45m#{val('d4')} \e[47m#{val('e4')} \e[45m#{val('f4')} \e[47m#{val('g4')} \e[45m#{val('h4')} \e[0m 4
-          3 \e[45m#{val('a3')} \e[47m#{val('b3')} \e[45m#{val('c3')} \e[47m#{val('d3')} \e[45m#{val('e3')} \e[47m#{val('f3')} \e[45m#{val('g3')} \e[47m#{val('h3')} \e[0m 3
-          2 \e[47m#{val('a2')} \e[45m#{val('b2')} \e[47m#{val('c2')} \e[45m#{val('d2')} \e[47m#{val('e2')} \e[45m#{val('f2')} \e[47m#{val('g2')} \e[45m#{val('h2')} \e[0m 2
-          1 \e[45m#{val('a1')} \e[47m#{val('b1')} \e[45m#{val('c1')} \e[47m#{val('d1')} \e[45m#{val('e1')} \e[47m#{val('f1')} \e[45m#{val('g1')} \e[47m#{val('h1')} \e[0m 1
+          8 \e[30m\e[47m#{val('a8')} \e[45m#{val('b8')} \e[47m#{val('c8')} \e[45m#{val('d8')} \e[47m#{val('e8')} \e[45m#{val('f8')} \e[47m#{val('g8')} \e[45m#{val('h8')} \e[0m 8
+          7 \e[30m\e[45m#{val('a7')} \e[47m#{val('b7')} \e[45m#{val('c7')} \e[47m#{val('d7')} \e[45m#{val('e7')} \e[47m#{val('f7')} \e[45m#{val('g7')} \e[47m#{val('h7')} \e[0m 7
+          6 \e[30m\e[47m#{val('a6')} \e[45m#{val('b6')} \e[47m#{val('c6')} \e[45m#{val('d6')} \e[47m#{val('e6')} \e[45m#{val('f6')} \e[47m#{val('g6')} \e[45m#{val('h6')} \e[0m 6
+          5 \e[30m\e[45m#{val('a5')} \e[47m#{val('b5')} \e[45m#{val('c5')} \e[47m#{val('d5')} \e[45m#{val('e5')} \e[47m#{val('f5')} \e[45m#{val('g5')} \e[47m#{val('h5')} \e[0m 5
+          4 \e[30m\e[47m#{val('a4')} \e[45m#{val('b4')} \e[47m#{val('c4')} \e[45m#{val('d4')} \e[47m#{val('e4')} \e[45m#{val('f4')} \e[47m#{val('g4')} \e[45m#{val('h4')} \e[0m 4
+          3 \e[30m\e[45m#{val('a3')} \e[47m#{val('b3')} \e[45m#{val('c3')} \e[47m#{val('d3')} \e[45m#{val('e3')} \e[47m#{val('f3')} \e[45m#{val('g3')} \e[47m#{val('h3')} \e[0m 3
+          2 \e[30m\e[47m#{val('a2')} \e[45m#{val('b2')} \e[47m#{val('c2')} \e[45m#{val('d2')} \e[47m#{val('e2')} \e[45m#{val('f2')} \e[47m#{val('g2')} \e[45m#{val('h2')} \e[0m 2
+          1 \e[30m\e[45m#{val('a1')} \e[47m#{val('b1')} \e[45m#{val('c1')} \e[47m#{val('d1')} \e[45m#{val('e1')} \e[47m#{val('f1')} \e[45m#{val('g1')} \e[47m#{val('h1')} \e[0m 1
             A B C D E F G H
     HEREDOC
   end
