d9e0a10fab208f1b891d1d70121d898d78078cdb
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-11-01 21:38:01 -0700

    feat: mark Rook and King as moved when appropriate

--- lib/chess.rb
+++ lib/chess.rb
@@ -96,7 +96,9 @@ class Chess
     destination = request_destination(selected_piece_location)
 
     game_board.move_piece(piece, selected_piece_location, destination)
-    piece.moved = true if piece.type == 'Pawn'
+    piece.moved = true if piece.type == 'Pawn' ||
+                          piece.type == 'Rook' ||
+                          piece.type == 'King'
 
     clear_legal_moves
     change_turn
