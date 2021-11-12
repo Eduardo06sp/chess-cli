19c14a77a60bab15aeaf58103f1250b71224ce68
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-11-12 06:06:58 -0800

    refactor: create new player oustide of #introduction by calling #create_player

--- lib/main.rb
+++ lib/main.rb
@@ -12,27 +12,9 @@ class Main
     input = validate_input(input, %w[load new], 'Please type "load" or "new".')
 
     if input == 'new'
-      puts 'Please enter a name for player one, or press enter to use the default.'
-      input = gets.chomp
-      p1_name = input == '' ? 'Player 1' : input
-
-      puts 'Please enter a name for player two, or press enter to use the default.'
-      input = gets.chomp
-      p2_name = input == '' ? 'Player 2' : input
-
-      puts 'Please enter a color for player one, or press enter to use the default (white).'
-      input = gets.chomp
-
-      until ['', 'white', 'black'].include?(input)
-        puts 'Please enter a color or press enter without typing.'
-        input = gets.chomp
-      end
-
-      p1_color = input == '' ? 'white' : input
-      p2_color = p1_color == 'white' ? 'black' : 'white'
-
-      p1 = Player.new(p1_name, p1_color)
-      p2 = Player.new(p2_name, p2_color)
+      p1 = create_player('one')
+      p2_color = p1.color == 'white' ? 'black' : 'white'
+      p2 = create_player('two', p2_color)
       new_game = Chess.new(p1, p2)
       new_game.add_initial_pieces
       new_game.play
