451ff62808071e7faafff1d02f34f33b7b4db39d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-11-18 00:51:30 -0800

    docs: explain Draw module

--- lib/draw.rb
+++ lib/draw.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Draw contains logic to end game in draw in the presence of
+# insufficient material or a stalemate
 module Draw
   def draw?
     insufficient_material?
